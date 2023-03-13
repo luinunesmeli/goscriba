8fd2adcb19a84516ee1e96a4af1e35e6a8888ebb
Luiz Nunes <luiz.nunes@mercadolivre.com>
2023-03-13 11:29:38 -0300

    Execute subcommands

---
+++ cmd/app/app.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"golang.org/x/oauth2"
+
+	"github.com/luinunesmeli/goscriba/scriba"
+	"github.com/luinunesmeli/goscriba/view"
+)
+
+func Run(cfg scriba.Config) error {
+	gitRepo, err := scriba.NewGitRepo(cfg)
+	if err != nil {
+		return err
+	}
+	owner, repo := gitRepo.GetRepoInfo()
+
+	github := scriba.NewGithubRepo(buildOauthclient(cfg), cfg, owner, repo)
+	changelog := scriba.NewChangelog(cfg.Changelog)
+
+	p := tea.NewProgram(view.NewView(&gitRepo, &github, &changelog, cfg))
+	if _, err = p.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildOauthclient(cfg scriba.Config) *http.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: cfg.GithubTokenAPI},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+	tc.Timeout = time.Second * 5
+
+	return tc
+}
