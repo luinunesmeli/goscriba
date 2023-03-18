124439ed59b2b32add5674c4d6110f5cd2899d49
Luiz Nunes <luiz.nunes@mercadolivre.com>
2023-03-18 14:11:58 -0300

    Move release calculation to another package

--- view/versionlist.go
+++ view/versionlist.go
@@ -2,11 +2,11 @@ package view
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/mritd/bubbles/common"
 	"github.com/mritd/bubbles/selector"
+
+	"github.com/luinunesmeli/goscriba/scriba"
 )
 
 type TypeMessage struct {
@@ -15,7 +15,7 @@ type TypeMessage struct {
 }
 
 func newVersionList(latestTag string) (*selector.Model, error) {
-	major, minor, patch, err := nextTags(latestTag)
+	major, minor, patch, err := scriba.NextReleases(latestTag)
 	if err != nil {
 		return nil, err
 	}
@@ -42,29 +42,4 @@ func newVersionList(latestTag string) (*selector.Model, error) {
 			return ""
 		},
 	}, nil
-}
-
-func nextTags(latestTag string) (string, string, string, error) {
-	parts := strings.Split(latestTag, ".")
-
-	patch, err := strconv.Atoi(parts[2])
-	if err != nil {
-		return "", "", "", err
-	}
-
-	minor, err := strconv.Atoi(parts[1])
-	if err != nil {
-		return "", "", "", err
-	}
-
-	major, err := strconv.Atoi(strings.TrimPrefix(parts[0], "v"))
-	if err != nil {
-		return "", "", "", err
-	}
-
-	versionFmt := "%d.%d.%d"
-	return fmt.Sprintf(versionFmt, major+1, minor, 0),
-		fmt.Sprintf(versionFmt, major, minor+1, 0),
-		fmt.Sprintf(versionFmt, major, minor, patch+1),
-		nil
 }
