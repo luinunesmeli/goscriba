1b4fb7eba654e21318796e0bf40089f753703769
Luiz Nunes <luiz.nunes@mercadolivre.com>
2023-03-18 14:15:04 -0300

    Update App

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -2,28 +2,31 @@ package app
 
 import (
 	"context"
-	"net/http"
+	"fmt"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/go-git/go-git/v5"
+	"github.com/google/go-github/v50/github"
 	"golang.org/x/oauth2"
 
+	"github.com/luinunesmeli/goscriba/pkg/auth"
 	"github.com/luinunesmeli/goscriba/pkg/config"
 	"github.com/luinunesmeli/goscriba/scriba"
 	"github.com/luinunesmeli/goscriba/view"
 )
 
 func Run(cfg config.Config) error {
-	gitRepo, err := scriba.NewGitRepo(cfg)
+	gitRepo, err := buildGitRepo(cfg)
 	if err != nil {
 		return err
 	}
-	owner, repo := gitRepo.GetRepoInfo()
+	owner, repoName := gitRepo.GetRepoInfo()
 
-	github := scriba.NewGithubRepo(buildOauthclient(cfg), cfg, owner, repo)
-	changelog := scriba.NewChangelog(cfg.Changelog)
+	githubClient := buildGithubClient(cfg, owner, repoName)
+	changelog := buildChangelog(cfg)
 
-	p := tea.NewProgram(view.NewView(&gitRepo, &github, &changelog, cfg))
+	p := tea.NewProgram(view.NewView(&gitRepo, &githubClient, &changelog, cfg))
 	if _, err = p.Run(); err != nil {
 		return err
 	}
@@ -31,12 +34,30 @@ func Run(cfg config.Config) error {
 	return nil
 }
 
-func buildOauthclient(cfg config.Config) *http.Client {
+func buildGitRepo(cfg config.Config) (scriba.GitRepo, error) {
+	repo, err := git.PlainOpen(cfg.Path)
+	if err != nil {
+		return scriba.GitRepo{}, fmt.Errorf("actual directory doesn't contains a git repository: %w", err)
+	}
+
+	gitRepo, err := scriba.NewGitRepo(repo, cfg, auth.AuthMethod(cfg))
+	if err != nil {
+		return scriba.GitRepo{}, err
+	}
+
+	return gitRepo, nil
+}
+
+func buildGithubClient(cfg config.Config, owner, repo string) scriba.GithubRepo {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: cfg.GetPersonalAccessToken()},
 	)
 	tc := oauth2.NewClient(context.Background(), ts)
 	tc.Timeout = time.Second * 5
 
-	return tc
+	return scriba.NewGithubRepo(github.NewClient(tc), cfg, owner, repo)
+}
+
+func buildChangelog(cfg config.Config) scriba.Changelog {
+	return scriba.NewChangelog(cfg.Changelog)
 }
